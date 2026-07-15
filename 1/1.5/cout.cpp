95bdbaef3d2d727b7cd68f4841ae20dffe234abc
nsvigash-lap <nsvigash1@gmail.com>
2026-07-15 21:52:06 +0530

    refactor: prints 'hey' additionally

--- 1/1.5/cout.cpp
+++ 1/1.5/cout.cpp
@@ -3,6 +3,6 @@
 int main() {
     int x{};
     // int x is default initialized to 0 in C++20
-    std::cout << "Number is: " << x ;
+    std::cout << "Number is: " << x << '\n' << "hey";
     return 0;
-}
+} 
